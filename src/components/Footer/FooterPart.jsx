66aaa9754a43e26b63d74b61096f5d554d7297ab
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2021-12-17 00:41:17 +0100

    init

--- src/components/Footer/FooterPart.jsx
+++ src/components/Footer/FooterPart.jsx
@@ -1,15 +1,44 @@
+import "./Footer.css";
+import { BsFacebook, BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs";
 const FooterPart = () => {
   return (
-    <div
-      className="d-flex align-items-center justify-content-center"
-      style={{ backgroundColor: "#454040", color: "white", height: "10vh" }}
-    >
-      <small> </small>
-      <small>
-        Conditions of Use & SalePrivacy Notice Interest-Based Ads Notice
-        ©2016-2021, strivebookstore.com, Inc. or its affiliates{" "}
-      </small>
-    </div>
+    <footer className="footer">
+      <div className="social-icons">
+        <BsFacebook className="mr-5 mb-5 social" />
+        <BsInstagram className="mr-5 mb-5 social" />
+        <BsTwitter className="mr-5 mb-5 social" />
+        <BsYoutube className="mr-5 mb-5 social" />
+      </div>
+
+      <div className="grid-footer">
+        <div className="footer-section">
+          <p>Audio and Subtitles</p>
+          <p>Media Centre</p>
+          <p>Privacy</p>
+          <p>Contact Us</p>
+        </div>
+
+        <div className="footer-section">
+          <p>Audio Description</p>
+          <p>Investor Relations</p>
+          <p>Legal Notices</p>
+        </div>
+
+        <div className="footer-section">
+          <p>Help Centre</p>
+          <p>Jobs</p>
+          <p>Cookie Preferences</p>
+        </div>
+
+        <div className="footer-section">
+          <p>Gift Cards</p>
+          <p>Terms of Use</p>
+          <p>Corporate Information</p>
+        </div>
+      </div>
+      <button className="btn-footer">Service Code</button>
+      <p className="p-footer">&copy; 1997-2021 Netflix, Inc.</p>
+    </footer>
   );
 };
 export default FooterPart;
--- src/components/MyNav/MyNav.jsx
+++ src/components/MyNav/MyNav.jsx
@@ -1,43 +1,93 @@
+import "./MyNav.css";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { IoCartOutline, IoSearch } from "react-icons/io5";
+
 const MyNav = () => {
   return (
-    <Navbar
-      bg="dark"
-      variant="dark"
-      className="sticky-top"
-      style={{ height: "70px", fontSize: "large" }}
-    >
-      <Navbar.Brand href="#home">Strive Library</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <NavDropdown title="Select Book Category" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Sci-fi</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Romantic</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Horror</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Fantacy</NavDropdown.Item>
+    <>
+      <nav className="navbar navbar-expand-lg nav-center">
+        <a className="navbar-brand" href="/home.html">
+          <img
+            src="netflix_logo.png"
+            className="nav-logo-img"
+            alt="netflix logo"
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">History</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-      <Nav className="mr-sm-2">
-        <IoSearch
-          style={{ color: "white", height: "30px", width: "30px" }}
-          className="mt-2 mr-5"
-        />
-        <IoCartOutline
-          style={{ color: "white", height: "30px", width: "30px" }}
-          className="mt-2 mr-3"
-        />
-        <Nav.Link href="#features">About</Nav.Link>
-        <Nav.Link href="#pricing">Browse</Nav.Link>
-      </Nav>
-      {/*  <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-light">Search</Button>
-      </Form> */}
-    </Navbar>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link nav-link-active" href="/home.html">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                TV Shows
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Movies
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Recently Added
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                My List
+              </a>
+            </li>
+          </ul>
+
+          <i className="fas fa-search"></i>
+          <a className="nav-link" href="/back-office.html?id=">
+            Admin
+          </a>
+          <i className="fas fa-bell"></i>
+          <img src="avatar.png" className="nav-avatar" alt="nav-avatar" />
+          <i className="fas fa-caret-down"></i>
+        </div>
+      </nav>
+
+      <header class="header">
+        <div
+          class="banner"
+          style={{ backgroundImage: `url("squid-game.jpg")` }}
+        >
+          <div class="banner-info">
+            <h1 class="banner-title">SQUID GAMES</h1>
+            <div class="banner-icons">
+              <button class="banner-btn">Play</button>
+              <button class="banner-btn">My List</button>
+            </div>
+            <div class="banner-description">
+              Hundreds of cash-strapped players accept a strange invitation to
+              compete in children's games. Inside, a tempting prize awaits —
+              with deadly high stakes.
+            </div>
+          </div>
+          <div class="banner-fade"></div>
+        </div>
+      </header>
+    </>
   );
 };
 export default MyNav;
