033719058ed9e17a6fa36c25b9a56821bad832e0
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2021-12-17 11:54:55 +0100

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,41 +3,7 @@ import { Container } from "react-bootstrap";
 
 class Home extends Component {
   state = {};
-  fetchCategories = async () => {
-    const response = await fetch(
-      "https://striveschool-api.herokuapp.com/api/movies/",
-      {
-        method: "GET",
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyODk0ZGFhY2FhMjAwMTU1MmExNjMiLCJpYXQiOjE2Mzg5NzQwMzgsImV4cCI6MTY0MDE4MzYzOH0.US5LnaBA-naCQfgPfcQYfFn8yEoEbO9Y468H2YPq7kw",
-        },
-      }
-    );
 
-    const categories = await response.json();
-    //console.log(categories);
-    return categories;
-  };
-  getMoviesByCategory = async (category) => {
-    let slideScrollLeft;
-    let slideScrollRight;
-    let raw = await fetch(
-      "https://striveschool-api.herokuapp.com/api/movies/" + category,
-      {
-        method: "GET",
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyODk0ZGFhY2FhMjAwMTU1MmExNjMiLCJpYXQiOjE2Mzg5NzQwMzgsImV4cCI6MTY0MDE4MzYzOH0.US5LnaBA-naCQfgPfcQYfFn8yEoEbO9Y468H2YPq7kw",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    let movies = await raw.json();
-    // console.log(movies);
-    /*     displayProducts(movies, category);
-     */
-  };
   render() {
     return (
       <>
